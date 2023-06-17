3c9e90add1168cc6ef15e8b1540268650e1feb96
BIGNIKi <nikita_sr1@mail.ru>
2023-06-18 00:52:03 +0700

    ATM state window

---
+++ TestTaskCadwise2/Models/BanknoteInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace TestTaskCadwise2.Models
+{
+    public class BanknoteInfo
+    {
+        public string Banknote { get; }
+        public string CountInfo
+        {
+            get
+            {
+                return $"{Count}/{Capacity}";
+            }
+        }
+
+        public int Capacity { get; }
+
+        public int Count { get; set; }
+
+        public BanknoteInfo( string banknote, int capacity, int initialCount )
+        {
+            Banknote = banknote; 
+            Capacity = capacity;
+            Count = initialCount;
+        }
+    }
+}
---
+++ TestTaskCadwise2/ViewModels/ATMStateViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using TestTaskCadwise2.Models;
+
+namespace TestTaskCadwise2.ViewModels
+{
+    public class ATMStateViewModel : ViewModelBase
+    {
+        private const int ATMCapacity = 2000;
+
+        public List<BanknoteInfo> Banknotes { get; set; }
+
+        public ATMStateViewModel()
+        {
+            Banknotes = new List<BanknoteInfo>()
+            {
+                new BanknoteInfo("10 ₽", ATMCapacity, 0),
+                new BanknoteInfo("50 ₽", ATMCapacity, 0),
+                new BanknoteInfo("100 ₽", ATMCapacity, 0),
+                new BanknoteInfo("500 ₽", ATMCapacity, 0),
+                new BanknoteInfo("1000 ₽", ATMCapacity, 0),
+                new BanknoteInfo("5000 ₽", ATMCapacity, 0)
+            };
+        }
+    }
+}
